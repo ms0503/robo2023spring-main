0b58954283458b4489462d32f7bd8ea36f3653e6
Sora Tonami <ms0503@outlook.com>
2023-03-06 10:26:55 +0900

    initial commit

---
+++ Core/Inc/params.hh
@@ -0,0 +1,19 @@
+//
+// Created by ms0503 on 23/03/03.
+//
+
+#ifndef MAIN_PARAMS_HH
+#define MAIN_PARAMS_HH
+
+#include <cstdint>
+
+// Addresses
+constexpr const uint8_t ADDR_MAIN = 0x00;
+constexpr const uint8_t ADDR_MOTOR_FL = 0x10;
+constexpr const uint8_t ADDR_MOTOR_FR = 0x11;
+constexpr const uint8_t ADDR_MOTOR_RL = 0x12;
+constexpr const uint8_t ADDR_MOTOR_RR = 0x13;
+
+// Sizes
+constexpr const uint16_t SBDBT_RECEIVE_SIZE = 8;
+#endif //MAIN_PARAMS_HH
---
+++ Core/Inc/types.hh
@@ -0,0 +1,8 @@
+//
+// Created by ms0503 on 23/03/05.
+//
+
+#ifndef MAIN_TYPES_HH
+#define MAIN_TYPES_HH
+
+#endif //MAIN_TYPES_HH
