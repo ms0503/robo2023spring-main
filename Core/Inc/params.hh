1fc26e509b78b68468ccd15797c56cc9f88a5062
Sora Tonami <ms0503@outlook.com>
2023-03-06 23:37:06 +0900

    モーターの動作用のプリセットとその型を追加。

--- Core/Inc/params.hh
+++ Core/Inc/params.hh
@@ -5,7 +5,10 @@
 #ifndef MAIN_PARAMS_HH
 #define MAIN_PARAMS_HH
 
+#include "types.hh"
 #include <cstdint>
+#include <map>
+#include <string>
 
 // Addresses
 constexpr const uint8_t ADDR_MAIN = 0x00;
@@ -16,4 +19,19 @@ constexpr const uint8_t ADDR_MOTOR_RR = 0x13;
 
 // Sizes
 constexpr const uint16_t SBDBT_RECEIVE_SIZE = 8;
+
+// Motor presets
+constexpr const MotorPreset MOTOR_PRESET_NORMAL {
+    .FL = -10,
+    .FR = -10,
+    .RL = 10,
+    .RR = 10
+};
+constexpr const MotorPreset MOTOR_PRESET_STOP {
+    .FL = 0,
+    .FR = 0,
+    .RL = 0,
+    .RR = 0
+};
+
 #endif //MAIN_PARAMS_HH
--- Core/Inc/types.hh
+++ Core/Inc/types.hh
@@ -5,4 +5,13 @@
 #ifndef MAIN_TYPES_HH
 #define MAIN_TYPES_HH
 
+#include <cstdint>
+
+typedef struct {
+    int32_t FL;
+    int32_t FR;
+    int32_t RL;
+    int32_t RR;
+} MotorPreset;
+
 #endif //MAIN_TYPES_HH
