43c8b07f804457e1c452c34e1483254d4bc40ee6
Sora Tonami <ms0503@outlook.com>
2023-03-08 17:38:28 +0900

    コメントを追加。

--- Core/Inc/Motor.hh
+++ Core/Inc/Motor.hh
@@ -9,25 +9,32 @@
 #include <map>
 #include <string>
 
+/// モーター信号
 typedef struct {
+    /// 左前方
     int32_t FL;
+    /// 右前方
     int32_t FR;
+    /// 左後方
     int32_t RL;
+    /// 右後方
     int32_t RR;
 } MotorState;
 
+/// モーターの最高速度
 constexpr const int32_t MOTOR_SPEED_MAX = 30;
 
+/// モーターの回転方向
 enum class EnumMotorRotate : int8_t {
     LEFT = -1,
     RIGHT = 1
 };
 
+/// モーター制御クラス
 class Motor {
 public:
     Motor();
     ~Motor();
-    static void rotate(int32_t speed);
 };
 
 #endif //MAIN_MOTOR_HH
