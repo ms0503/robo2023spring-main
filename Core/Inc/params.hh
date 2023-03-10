e277b871a0c87e705eda14a0763316ada0043036
Sora Tonami <ms0503@outlook.com>
2023-03-10 11:24:36 +0900

    パラメータ類をカテゴリごとに折り畳めるよう変更。

--- Core/Inc/params.hh
+++ Core/Inc/params.hh
@@ -8,18 +8,34 @@
 #include "stm32f407xx.h"
 #include <cstdint>
 
-// Addresses
+// <editor-fold desc="CANアドレス">
+/// メインボードのCANアドレス
 constexpr const uint8_t ADDR_MAIN = 0x00;
+/// 左前方のホイールのCANアドレス
 constexpr const uint8_t ADDR_MOTOR_FL = 0x10;
+/// 右前方のホイールのCANアドレス
 constexpr const uint8_t ADDR_MOTOR_FR = 0x11;
+/// 左後方のホイールのCANアドレス
 constexpr const uint8_t ADDR_MOTOR_RL = 0x12;
+/// 右後方のホイールのCANアドレス
 constexpr const uint8_t ADDR_MOTOR_RR = 0x13;
+// </editor-fold>
 
-// Pins
+// <editor-fold desc="GPIOピン">
+/// 回収機構のピン
 constexpr const uint16_t PIN_AIR_CYLINDER_ARM = 0;
+/// 射出機構のピン
 constexpr const uint16_t PIN_AIR_CYLINDER_THROWER = 1;
+// </editor-fold>
 
-// Sizes
+// <editor-fold desc="データサイズ">
+/// SBDBTのデータサイズ
 constexpr const uint16_t SBDBT_RECEIVE_SIZE = 8;
+// </editor-fold>
+
+// <editor-fold desc="その他パラメータ">
+/// モーターの最高速度
+constexpr const int32_t MOTOR_SPEED_MAX = 30;
+// </editor-fold>
 
 #endif //MAIN_PARAMS_HH
