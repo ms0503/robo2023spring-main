ba6de7d5ee17e1d766d839dcf089dd3f471cd899
Sora Tonami <ms0503@outlook.com>
2023-03-20 10:50:53 +0900

    pre-commitフックをlint時にfixがあった場合コミットを一旦中断するよう変更。

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -8,11 +8,13 @@ if ! (type "clang-format" &>/dev/null); then
     printf "Please install clang-format.\n"
     exit 1
 fi
+fixed=0
 for file in $(git diff --cached --name-only --diff-filter=AM "$against" -- | grep -E '\.(c(c|pp|xx)?|h(h|pp)?)$'); do
     diff -q "$file" <(clang-format "$file") &>/dev/null && continue
+    fixed=1
     printf "[1m%s[0m\n" "$file"
     diff --color=always -u "$file" <(clang-format "$file") | sed -e '1,2d'
     printf "\n\n"
     clang-format -i "$file"
 done
-exit 0
+exit $fixed
